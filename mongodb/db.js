ad2392085fb971ba33613e76933160b886ee0919
Mads <maller2650@gmail.com>
2021-05-18 18:53:19 +0200

    added comments to db.js

--- mongodb/db.js
+++ mongodb/db.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 
 // https://mrvautin.com/re-use-mongodb-database-connection-in-routes/
+
+// variables to be used
 const mongoClient = require("mongodb").MongoClient;
 const url = "mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
 const dbName = process.env.DB_NAME;
 let mongodb;
 
+// wraps the server.listen call in app.js
+// this makes it so the connection can be re-used through the application
+// and helps by not having to call mongoClient.connect... every time a db operation needs to be performed
 function connect(callback) {
     mongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         mongodb = client.db(dbName);
@@ -13,10 +18,14 @@ function connect(callback) {
     });
 }
 
+// utility function to call when doing operations
+// eg. db.query().collection('users').find({}).toArray()
 function query() {
     return mongodb;
 }
 
+// utility function to close connection
+// not really sure if needed but keeping it for now
 function close() {
     mongodb.close();
 }
