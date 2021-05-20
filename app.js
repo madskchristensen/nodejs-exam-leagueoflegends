0873a5b88047d4299b0fa089c785d5ec9f854d2a
Michael Fuglø <michael@fuglo.com>
2021-05-20 20:02:52 +0200

    added express-session to simulate login functionality and display pages dynamically. Also added basic html setup for later usaage

--- app.js
+++ app.js
@@ -27,8 +27,39 @@ const io = require("socket.io")(server);
 // escapehtml setup
 const escapeHTML = require("html-escaper").escape;
 
+// node-fetch setup
+const fetch = require("node-fetch");
+
+// session setup
+// create session 
+const session = require("express-session");
+
+app.use(session({
+    secret: process.env.SESSION_SECRET.split(","), // used to compute hash. split to process dotenv variable as array
+    name: process.env.SESSION_NAME, // hidden custom name to avoid fingerprinting
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false, // dont require HTTPS connection
+        sameSite: true, // block CORS req
+        maxAge: 600000 // Time in miliseconds - 10 minutes
+    }
+}));
+
+const sessionRouter = require("./router/session.js");
+app.use(sessionRouter.router);
+
+const fs = require('fs');
+const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8")
+const login = fs.readFileSync(__dirname + "/public/login/login.html", "utf-8")
+
+
 app.get("/", (req, res) => {
-    res.send("index");
+    res.send(frontpage);
+})
+
+app.get("/login", (req, res) => {
+    res.send(login);
 })
 
 // listen at specified port
@@ -36,6 +67,6 @@ server.listen(port, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("Express listening at port", port);
+        console.log("Express listening at port ", port);
     }
 })
---
+++ public/frontpage/frontpage.js
@@ -0,0 +1,48 @@
+  // const express = require("express").Router;
+  //const router = express.Router();
+
+(async function getSession() {
+    try {
+        const response = await fetch("/getSession");
+        const result = await response.json();
+
+        console.log(result);
+
+        // create frontpage
+        const frontpageDiv = document.getElementById("frontpage");
+
+        const message = document.createElement("p");
+
+        if (result.session.loggedIn === true) {
+            console.log(result.session.loggedIn);
+            message.innerText = "You are logged in"
+            
+            const signoutForm = document.createElement("form");
+            signoutForm.method = "POST";
+            signoutForm.action = "/api/signout";
+
+            const signoutButton = document.createElement("input");
+            signoutButton.type = "submit";
+            signoutButton.value = "Sign out";
+
+            signoutForm.appendChild(signoutButton);
+            frontpageDiv.appendChild(signoutForm);
+        }
+        else if (result.session.loggedIn === false || !result.session.loggedIn) {
+            console.log(result.session.loggedIn);
+            message.innerText = "You are not logged in"
+
+            const loginLink = document.createElement("a")
+            loginLink.innerText = "Login"
+            loginLink.href = "/login"
+            frontpageDiv.appendChild(loginLink)
+        }
+
+        frontpageDiv.appendChild(message);
+
+    }
+    catch(error) {
+        console.log(error);
+    }
+})();
+
