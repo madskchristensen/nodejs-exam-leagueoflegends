824baae8b1dac4f33a91976dfbdcfca80bb6df9f
Mads <maller2650@gmail.com>
2021-06-04 16:10:09 +0200

    created basic search functionality. No error handling yet and case sensitive.

--- public/frontpage/frontpage.js
+++ public/frontpage/frontpage.js
@@ -1,5 +1,13 @@
-const searchInput = document.getElementById("search-input");
+(function initializeSearchListener() {
+    const searchInput = document.getElementById("search-input");
+    const regionSelect = document.getElementById("select-region");
 
-searchInput.addEventListener("input", () => {
-    console.log(searchInput.value);
-})
+    searchInput.addEventListener("search", () => {
+        const region = regionSelect.value.toLowerCase();
+        const summonerName = searchInput.value;
+        const currentUrl = location.href;
+
+        location.href = currentUrl + "profile/" + summonerName + "/" + region;
+    })
+
+})();
